cc90b34b8bb5970f99e24c5163a5227400b184d6
Nikhil chawla <nikhil.chawla@simplifyvms.com>
2023-08-14 23:01:05 +0530

    added delete fnc & video el in gallery

--- gallery.js
+++ gallery.js
@@ -32,8 +32,62 @@ function dbCheck() {
           <div class="cursor-pointer download action-btn">DOWNLOAD</div>
           `;
           gallery_cont.append(imageEl);
+          console.log(imageEl,' pl');
+          const delete_btn = imageEl.querySelector(".delete");
+          delete_btn.addEventListener("click",()=>{
+            deleteFnc(image?.id);
+          })
+        })
+      }
+    }
+
+    let videoTransaction = db.transaction("video", "readonly"); // (1)
+    // get an object store to operate on it
+    let videoStore = videoTransaction.objectStore("video"); // (2)
+    let videoReq = videoStore.getAll();
+    videoReq.onsuccess = function () {
+      if (videoReq.result != undefined) {
+        console.log(videoReq.result, ' get images');
+        const gallery_cont = document.querySelector(".gallery-cont");
+        const videoResult = videoReq.result;
+        videoResult.forEach((video) => {
+          // console.log(image);
+          const blobData = video?.blobData;
+          const videoUrl = URL.createObjectURL(blobData);
+          // const imageId = image?.id;
+          const videoEl = document.createElement("div");
+          videoEl.setAttribute("class", "media-cont")
+          videoEl.innerHTML = `
+          <div class="media">
+          <video autoplay loop muted src="${videoUrl}"/>
+          </div>
+          <div class="cursor-pointer delete action-btn">DELETE</div>
+          <div class="cursor-pointer download action-btn">DOWNLOAD</div>
+          `;
+          gallery_cont.append(videoEl);
+          // console.log(videoEl,' pl');
+          const delete_btn = videoEl.querySelector(".delete");
+          delete_btn.addEventListener("click",()=>{
+            deleteFnc(video?.id, 'video');
+          })
         })
       }
     }
   }
+}
+
+function deleteFnc(mediaId,mediaType = 'image') {
+  // console.log(mediaId,"delete fnc called");
+  if(mediaType == 'image') {
+    const imageTransaction = db.transaction("image", "readwrite"); // (1)
+    const imageStore = imageTransaction.objectStore("image");
+    imageStore.delete(mediaId);
+  } else {
+    const videoTransaction = db.transaction("video", "readwrite"); // (1)
+    const videoStore = videoTransaction.objectStore("video");
+    videoStore.delete(mediaId);
+  }
+  const gallery_cont = document.querySelectorAll(".gallery-cont > *");
+  gallery_cont.forEach((mediaEl)=>mediaEl.remove());
+  dbCheck();
 }
